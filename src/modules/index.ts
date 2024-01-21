f30c7200c7eba93417816354274e865110c8201a
Batur Akkurt <halidbaturakkurt@gmail.com>
2024-01-21 14:25:51 +0300

    feat: sentry

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,9 +3,12 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
+import { SentryModule } from '@ntegral/nestjs-sentry';
+
 import { PrismaModule } from './prisma';
 import { TodoModule } from './todo';
 
+import packageJson from '../../package.json';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -13,6 +16,13 @@ import { TodoModule } from './todo';
       playground: true,
       autoSchemaFile: 'schema.gql',
       sortSchema: true,
+    }),
+    SentryModule.forRoot({
+      dsn: process.env.SENTRY_DSN,
+      debug: process.env.NODE_ENV !== 'production',
+      environment: process.env.NODE_ENV,
+      release: packageJson.version,
+      logLevels: ['debug'],
     }),
     PrismaModule,
     TodoModule,
